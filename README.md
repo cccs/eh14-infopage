a971555c859be62c40557726bf2535c56ecf239d
Stefan Schlott <stefan@ploing.de>
2013-04-21 15:17:04 +0200

    Parallax scroll effect

--- README.md
+++ README.md
@@ -5,4 +5,6 @@
 * JQuery
 * [scrollTo plugin](http://github.com/yckart/jquery.scrollto.js)
 * [One page nav plugin](https://github.com/davist11/jQuery-One-Page-Nav)
+* Parallax scroll derived from
+  [this tutorial](http://www.franckmaurin.com/the-parallax-effects-with-jquery/)
 
---
+++ javascripts/jquery.parallax.js
@@ -0,0 +1,25 @@
+;(function($){
+    $.fn.parallax = function(options){
+        var $$ = $(this);
+        offset = $$.offset();
+        var defaults = {
+            "start": 0,
+            "stop": offset.top + $$.height(),
+            "coeff": 0.95
+        };
+        var opts = $.extend(defaults, options);
+        return this.each(function(){
+            $(window).bind('scroll', function() {
+                windowTop = $(window).scrollTop();
+                if((windowTop >= opts.start) && (windowTop <= opts.stop)) {
+                    newCoord = Math.round(windowTop * opts.coeff);
+                    $$.css({
+                        "top": newCoord + "px"
+                    });
+                    console.log("new top: " + newCoord + "px")
+                    console.log($$.css('top'))
+                }
+            });
+        });
+    };
+})(jQuery);
